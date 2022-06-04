3f2ef80da16d08aadf484f0f08cbc42f8454b486
Aadil Srivastava <aadil.srivastava01@gmail.com>
2022-06-05 01:00:38 +0530

    updated_functionality_hashmap

--- create_custom_hash.cpp
+++ create_custom_hash.cpp
@@ -7,20 +7,29 @@ Problem Link: Creating Custom HashTable
 #include <algorithm>
 #include <iostream>
 #include <string>
+#include <utility>
 #include <vector>
 
 class CustomHashmap {
  private:
   int size;
-  int *p{nullptr};
+  int hash_size;
+  std::pair<std::string, int> *ptr{nullptr};
+  int *fill_idx_ptr{nullptr};
 
  public:
-  int _hash(const std::string &key) const;
   CustomHashmap(int hash_space);
-  ~CustomHashmap() { delete[] p; }
-  int get(const std::string &key);
+  ~CustomHashmap() { delete[] ptr; }
+  int _hash(const std::string &key) const;
+  auto get(const std::string &key) const;
   void set(const std::string &key, int value);
+  void keys() const;
 };
+
+CustomHashmap::CustomHashmap(int hash_space) : size{hash_space}, hash_size{0} {
+  ptr = new std::pair<std::string, int>[size];
+  fill_idx_ptr = new int[size];
+}
 
 int CustomHashmap::_hash(const std::string &key) const {
   int hash{0};
@@ -30,24 +39,32 @@ int CustomHashmap::_hash(const std::string &key) const {
   }
   return hash;
 }
-CustomHashmap::CustomHashmap(int hash_space) : size{hash_space} {
-  p = new int[size];
+
+auto CustomHashmap::get(const std::string &key) const {
+  int idx = _hash(key);
+  return *(ptr + idx);
 }
 
-int CustomHashmap::get(const std::string &key) {
-  int idx = _hash(key);
-  return *(p + idx);
-}
 void CustomHashmap::set(const std::string &key, int value) {
   int idx = _hash(key);
-  *(p + idx) = value;
+  ptr[idx] = std::pair<std::string, int>{key, value};
+  fill_idx_ptr[hash_size] = idx;
+  hash_size++;
 }
 
+void CustomHashmap::keys() const {
+  for (int i = 0; i < hash_size; i++) {
+    std::cout << (ptr + fill_idx_ptr[i])->first << ": ";
+    std::cout << (ptr + fill_idx_ptr[i])->second << "\n";
+  }
+  std::cout << std::endl;
+}
 int main() {
-    CustomHashmap h1{50};
-    std::cout << h1._hash("grapess");
-//   std::string s1{"aadil"};
-//   std::cout << (int)s1.at(2);
+  CustomHashmap h1{50};
+  h1.set("grapes", 1000);
+  h1.set("Orages", 11);
+  h1.set("apple", 15);
+  h1.keys();
   std::cout << std::endl;
   return 0;
 }
