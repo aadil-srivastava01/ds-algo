fe5dbbe92cc19ae7d064220b55de909b451838a8
Aadil Srivastava <aadil.srivastava01@gmail.com>
2022-06-08 17:40:12 +0530

    completed_BST

--- binary_search_tree.cpp
+++ binary_search_tree.cpp
@@ -12,6 +12,7 @@ Problem Link: Create a Binary Search Tree
 class TreeNode {
  public:
   friend std::ostream &operator<<(std::ostream &os, const TreeNode &obj);
+  friend std::ostream &operator<<(std::ostream &os, const TreeNode *obj);
   int val;
   TreeNode *left;
   TreeNode *right;
@@ -22,31 +23,40 @@ std::ostream &operator<<(std::ostream &os, const TreeNode &obj) {
   os << obj.val;
   return os;
 }
+std::ostream &operator<<(std::ostream &os, const TreeNode *obj) {
+  if (obj == NULL)
+    os << "Not_Found";
+  else
+    os << obj->val;
+  return os;
+}
 
 class BinarySearchTree {
  private:
   TreeNode *parent;
   void iTraverse(TreeNode *node);
+  bool isleaf(TreeNode *node);
 
  public:
   TreeNode *root;
 
   BinarySearchTree(int x);
-  TreeNode lookup(int x);
+  TreeNode *lookup(int x);
   void insert(int x);
   void remove(int x);
   void inorder_traverse() { iTraverse(root); };
 };
 
 BinarySearchTree::BinarySearchTree(int x) { root = new TreeNode{x}; }
-TreeNode BinarySearchTree::lookup(int x) {
+
+TreeNode *BinarySearchTree::lookup(int x) {
   parent = root;
   auto curr = root;
 
   while (curr != NULL) {
     int curr_val = curr->val;
     if (x == curr_val)
-      return *curr;
+      return curr;
     else if (x < curr_val) {
       parent = curr;
       curr = curr->left;
@@ -55,9 +65,7 @@ TreeNode BinarySearchTree::lookup(int x) {
       curr = curr->right;
     }
   }
-  if (curr == NULL) {
-    return -1;
-  }
+  return NULL;
 }
 
 void BinarySearchTree::insert(int x) {
@@ -74,6 +82,71 @@ void BinarySearchTree::iTraverse(TreeNode *node) {
   if (node->right != NULL) iTraverse(node->right);
 }
 
+bool BinarySearchTree::isleaf(TreeNode *node) {
+  if (node->left == NULL && node->right == NULL) return true;
+  return false;
+}
+
+void BinarySearchTree::remove(int x) {
+  auto curr = lookup(x);
+  if (isleaf(curr)) {
+    if (parent->left == curr)
+      parent->left = NULL;
+    else
+      parent->right = NULL;
+    curr = NULL;
+    delete curr;
+  } else if (curr->right == NULL && curr->left != NULL) {
+    if (parent->left == curr)
+      parent->left = curr->left;
+    else
+      parent->right = curr->left;
+    curr = NULL;
+    delete curr;
+  } else {
+    auto tmp = curr->right;
+    while (tmp->left != NULL) tmp = tmp->left;
+
+    if (isleaf(tmp)) {
+      if (parent->left == curr)
+        parent->left = tmp;
+      else
+        parent->right = tmp;
+
+      curr = NULL;
+      tmp = NULL;
+      delete curr;
+      delete tmp;
+    }
+
+    else {
+      if (curr->right == tmp) {
+        tmp->left = curr->left;
+        if (parent->left == curr)
+          parent->left = tmp;
+        else
+          parent->right = tmp;
+        curr = NULL;
+        tmp = NULL;
+        delete curr;
+        delete tmp;
+      } else {
+        curr->right->left = tmp->right;
+        if (parent->left == curr)
+          parent->left = tmp;
+        else
+          parent->right = tmp;
+        tmp->right = curr->right;
+        tmp->left = curr->left;
+        tmp = NULL;
+        curr = NULL;
+        delete curr;
+        delete tmp;
+      }
+    }
+  }
+}
+
 int main() {
   BinarySearchTree bst{8};
   bst.insert(5);
@@ -86,6 +159,10 @@ int main() {
   bst.insert(10);
   bst.insert(14);
   std::cout << bst.lookup(21) << std::endl;
+  bst.inorder_traverse();
+  std::cout << std::endl;
+
+  bst.remove(6);
   bst.inorder_traverse();
   std::cout << std::endl;
   return 0;
