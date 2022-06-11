1d63deaae72c61ebdf0eef4633c995da815436bc
Aadil Srivastava <aadil.srivastava01@gmail.com>
2022-06-11 16:01:49 +0530

    improved_string_rev_recursion

--- reverse_string_recurse.cpp
+++ reverse_string_recurse.cpp
@@ -8,15 +8,14 @@ Problem Link: Reverse a string via recursion
 #include <iostream>
 #include <string>
 #include <vector>
-void rev(std::string &str, int i, int len) {
-  if (i >= len) return;
-  rev(str, i + 1, len);
-  std::cout << str[i];
+std::string rev(std::string &str, int i, int len) {
+  if (i >= len) return " ";
+  return rev(str, i + 1, len) + str[i];
 }
 
 int main() {
   std::string s1{"aadil"};
-  rev(s1, 0, s1.length());
+  std::cout << rev(s1, 0, s1.length());
   std::cout << std::endl;
   return 0;
 }
