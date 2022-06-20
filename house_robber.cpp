644721598da45d02d2e99953027c0b9b307db3d8
Aadil Srivastava <aadil.srivastava01@gmail.com>
2022-06-20 09:28:12 +0530

    memoize_house_robber

--- house_robber.cpp
+++ house_robber.cpp
@@ -5,30 +5,33 @@ Problem Link: https://leetcode.com/problems/house-robber/
 */
 
 #include <algorithm>
-#include <cstdlib>
 #include <iostream>
 #include <string>
-#include <unordered_map>
 #include <vector>
 
-// class Solution {
-//  public:
-//   void stratergise(auto& table, std::vector<int>& nums);
-//   int rob(std::vector<int>& nums) { int size = nums.size() - 1; }
-// };
-
-int Kadane(std::vector<int>& vec) {
-  int local_max{0}, global_max{INT_MIN};
-  for (int i = 0; i < vec.size(); i++) {
-    local_max = std::max(vec[i], vec[i] + local_max);
-    if (local_max > global_max) global_max = local_max;
+class Solution {
+ public:
+  int rob_smartly(std::vector<int>& nums, int size, int idx,
+                  std::vector<int>& dp) {
+    if (idx >= size)
+      return 0;
+    else if (dp[idx] != -1) {
+      return dp[idx];
+    }
+    return std::max(nums[idx] + rob_smartly(nums, size, idx + 2, dp),
+                    rob_smartly(nums, size, idx + 1, dp));
   }
-  return global_max;
-}
+  int rob(std::vector<int>& nums) {
+    int size = nums.size();
+    std::vector<int> dp(size + 1, -1);
+    return rob_smartly(nums, size, 0, dp);
+  }
+};
 
 int main() {
-  std::vector<int> v1{-2, -3, 4, -1, -2, 1, 5, -3};
-  std::cout << Kadane(v1);
+  std::vector<int> v1{8, 9, 3, 1, 1, 2};
+  Solution s1;
+  std::cout << s1.rob(v1);
   std::cout << std::endl;
 
   return 0;
