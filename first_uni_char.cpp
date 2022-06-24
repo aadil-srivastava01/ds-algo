fd7dd3b0507424fc7f2a7120477601232828c4d6
Aadil Srivastava <aadil.srivastava01@gmail.com>
2022-06-24 09:32:31 +0530

    improved_solution

--- first_uni_char.cpp
+++ first_uni_char.cpp
@@ -11,7 +11,7 @@ Problem Link: https://leetcode.com/problems/first-unique-character-in-a-string/
 #include <utility>
 #include <vector>
 
-class Solution {
+class Inefficient_Solution {
  public:
   int firstUniqChar(std::string s) {
     if (s.size() == 1) return 0;
@@ -38,10 +38,24 @@ class Solution {
   }
 };
 
+class Solution {
+ public:
+  int firstUniqChar(std::string s) {
+    std::vector<size_t> v1(26, 0);
+    for (const char &c : s) v1[c - 'a'] += 1;
+    for (int i = 0; i < s.size(); i++) {
+      if (v1.at(s[i] - 'a') == 1) return i;
+    }
+    return -1;
+  }
+};
+
 int main() {
-  std::string s1{"leetcode"};
-  Solution s;
-  std::cout << s.firstUniqChar(s1);
+  std::string s1{"aabb"};
+  Inefficient_Solution s;
+  Solution s2;
+  std::cout << s.firstUniqChar(s1) << std::endl;
+  std::cout << s2.firstUniqChar(s1);
   std::cout << std::endl;
   return 0;
 }
