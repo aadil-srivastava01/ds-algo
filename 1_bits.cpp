1d13b8b6e8bca21d2da7444a0bab9c651ec7cbb6
Aadil Srivastava <aadil.srivastava01@gmail.com>
2022-06-28 18:56:50 +0530

    added_new_solution

---
+++ 1_bits.cpp
@@ -0,0 +1,29 @@
+/*
+
+Problem Link: https://leetcode.com/problems/number-of-1-bits/
+
+*/
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Solution {
+ public:
+  int hammingWeight(uint32_t n) {
+    unsigned int count = 0;
+    while (n) {
+      count += n & 1;
+      n >>= 1;
+    }
+    return count;
+  }
+};
+
+int main() {
+  Solution s;
+  std::cout << s.hammingWeight(11111111111111111111111111111101);
+  std::cout << std::endl;
+  return 0;
+}
