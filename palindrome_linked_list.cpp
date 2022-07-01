13dc7dde8b793a231eae1a2a8adf400e7e627b9e
Aadil Srivastava <aadil.srivastava01@gmail.com>
2022-07-01 08:56:25 +0530

    updated_a_solution

--- palindrome_linked_list.cpp
+++ palindrome_linked_list.cpp
@@ -42,7 +42,68 @@ class Solution {
   }
 };
 
+// Exploiting a common fact used by Flyod's Cycle detection algorithm
+class SmartSolution {
+ public:
+  bool isPalindrome(ListNode *head) {
+    if (head->next == NULL) return true;
+    auto *fast{head};
+    auto *slow{head};
+    // Idea: When fast reaches END, slow will be at mid
+    while (fast->next != NULL) {
+      fast = fast->next;
+      if (fast->next != NULL) {
+        fast = fast->next;
+        slow = slow->next;
+      }
+    }
+    // Reversing the 2nd half of the list
+    auto prev{slow};
+    auto mid{slow};
+    auto tmp{slow};
+    bool mark_end{false};
+    slow = slow->next;
+    while (slow != NULL) {
+      prev = slow;
+      slow = slow->next;
+      prev->next = tmp;
+      tmp = prev;
+      if (!mark_end) {
+        prev->next = NULL;
+        mid->next = NULL;
+        mark_end = true;
+      }
+    }
+    tmp = head;
+    std::cout << mid->val << "$$$$" << std::endl;
+
+    while (prev != NULL) {
+      std::cout << tmp->val << " " << prev->val << std::endl;
+      if (tmp->val != prev->val) return false;
+      tmp = tmp->next;
+      prev = prev->next;
+    }
+    return true;
+  }
+};
+
 int main() {
+  ListNode l1(1);
+  ListNode l2(2);
+  ListNode l3(3);
+  ListNode l6(1);
+  ListNode l5(2);
+  ListNode l4(3);
+
+  //   l5.next = &l6;
+  l4.next = &l5;
+  l3.next = &l4;
+  l2.next = &l3;
+  l1.next = &l2;
+
+  SmartSolution s1;
+  std::cout << s1.isPalindrome(&l1);
+
   std::cout << std::endl;
   return 0;
 }
