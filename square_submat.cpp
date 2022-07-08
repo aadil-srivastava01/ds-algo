d3de9c944b2a66b49a915c80fafb737341edfa5c
Aadil Srivastava <aadil.srivastava01@gmail.com>
2022-07-08 10:26:02 +0530

    updated_solution

--- square_submat.cpp
+++ square_submat.cpp
@@ -12,7 +12,30 @@ https://leetcode.com/problems/count-square-submatrices-with-all-ones/
 
 class Solution {
  public:
-  int countSquares(std::vector<std::vector<int>>& matrix) {}
+  int countSquares(std::vector<std::vector<int>>& matrix) {
+    int rows = matrix.size();
+    int columns = matrix[0].size();
+    int tmp{0};
+    int counter{0}, min_v{0};
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < columns; j++) {
+        if (i == 0 || j == 0) {
+          counter += matrix[i][j];
+          continue;
+        }
+        tmp = matrix[i][j];
+        if (tmp) {
+          min_v = std::min(matrix[i - 1][j - 1],
+                           std::min(matrix[i - 1][j], matrix[i][j - 1]));
+          tmp = min_v + 1;
+          counter += tmp;
+          matrix[i][j] = tmp;
+        } else
+          continue;
+      }
+    }
+    return counter;
+  }
 };
 
 int main() {
