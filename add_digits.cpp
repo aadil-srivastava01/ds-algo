9528e7522a1f3a1a5524f5d78bb624fa5d8bcff7
Aadil Srivastava <aadil.srivastava01@gmail.com>
2022-07-10 22:55:39 +0530

    added_new_solution

---
+++ add_digits.cpp
@@ -0,0 +1,30 @@
+/*
+
+Problem Link: https://leetcode.com/problems/add-digits/
+
+*/
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+/*
+Applyting Congruence Formula
+ digit_sum = {
+
+                0 is n = 0
+                1 + (n-1) mod (base-1) n!=0
+ }
+ */
+class Solution {
+ public:
+  int addDigits(int num) {
+    if (num == 0) return 0;
+    return (1 + ((num - 1) % (9)));
+  }
+};
+
+int main() {
+  std::cout << std::endl;
+  return 0;
+}
