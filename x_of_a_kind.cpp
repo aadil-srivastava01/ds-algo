5277f58232e986717bf2fc562c7da458ab6a9737
Aadil Srivastava <aadil.srivastava01@gmail.com>
2022-07-12 20:15:17 +0530

    updated_solution

--- x_of_a_kind.cpp
+++ x_of_a_kind.cpp
@@ -13,18 +13,27 @@ Problem Link: https://leetcode.com/problems/x-of-a-kind-in-a-deck-of-cards/
 class Solution {
  public:
   bool hasGroupsSizeX(std::vector<int>& deck) {
+    if (deck.size() < 2) return false;
     std::unordered_map<int, int> counter;
     for (const int& item : deck) {
       counter[item] += 1;
     }
-    
+    int prev = counter[deck.at(0)];
+    int curr{0};
+    for (auto i = counter.begin(); i != counter.end(); i++) {
+      if (prev == 1) return false;
+      curr = i->second;
+      if (curr != prev) return false;
+      prev = curr;
+    }
+    return true;
   }
 };
 
 int main() {
   Solution s1;
-  std::vector<int> v{1, 2, 3, 4, 4, 3, 2, 1};
-  s1.hasGroupsSizeX(v);
+  std::vector<int> v{1, 1, 2, 2, 2, 2};
+  std::cout << s1.hasGroupsSizeX(v);
   std::cout << std::endl;
   return 0;
 }
