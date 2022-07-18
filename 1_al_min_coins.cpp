3f11fdd58f9224dafd972af2164049b03edf80ad
Aadil Srivastava <aadil.srivastava01@gmail.com>
2022-07-19 00:19:45 +0530

    added_problem_descpition

--- 1_al_min_coins.cpp
+++ 1_al_min_coins.cpp
@@ -1,7 +1,14 @@
 /*
 
 Problem Link:
-
+Given an array of positive integers representing coin denominations and a single
+non-negative integer n representing a target amount of money, write a function
+that returns the smallest number of coins needed to make change for (to sum up
+to) that target amount using the given coin denominations.
+Note that you have access to an unlimited amount of coins.
+In other words, if the denominations are
+[1, 5, 10], you have access to an unlimited amount of 1 s, 5 s, and 10 s. If
+it's impossible to make change for the target amount, return -1
 */
 
 #include <algorithm>
@@ -30,7 +37,7 @@ int minNumberOfCoinsForChange(int n, std::vector<int> denoms) {
 }
 
 int main() {
-  std::vector<int> v1{1,2 };
+  std::vector<int> v1{1, 2};
   std::cout << minNumberOfCoinsForChange(3, v1);
   std::cout << std::endl;
   return 0;
