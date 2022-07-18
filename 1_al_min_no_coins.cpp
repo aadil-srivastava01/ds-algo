c67422dc4811db56137bfd79dc4eb0474393d33f
Aadil Srivastava <aadil.srivastava01@gmail.com>
2022-07-18 18:13:02 +0530

    removed_partially_correct_solution

--- 1_al_min_no_coins.cpp
+++ 1_al_min_no_coins.cpp
@@ -9,28 +9,7 @@ Problem Link:
 #include <string>
 #include <vector>
 
-int coinsOfADenom(int amount, int denom, int &noCoins) {
-  while (amount >= denom) {
-    noCoins++;
-    amount -= denom;
-  }
-  return amount;
-}
-
-int minNumberOfCoinsForChange(int n, std::vector<int> denoms) {
-  // Write your code here.
-  std::sort(denoms.begin(), denoms.end(), [](int a, int b) { return a > b; });
-  int remaningAmount{n}, noCoins{0};
-  for (const int &denom : denoms) {
-    if (denom > remaningAmount)
-      continue;
-    else if (remaningAmount == 0)
-      break;
-    remaningAmount = coinsOfADenom(remaningAmount, denom, noCoins);
-  }
-  if (remaningAmount == 0) return noCoins;
-  return -1;
-}
+int minNumberOfCoinsForChange(int n, std::vector<int> denoms) {}
 
 int main() {
   std::vector<int> v1{3, 5};
