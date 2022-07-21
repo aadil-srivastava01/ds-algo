666d92c34d111bc4ed57aeca6ea5c7e85bf31eb8
Aadil Srivastava <aadil.srivastava01@gmail.com>
2022-07-21 23:43:08 +0530

    removed_incorrect_solution

--- 1_al_levinstine.cpp
+++ 1_al_levinstine.cpp
@@ -18,23 +18,7 @@ Sample Output
 #include <string>
 #include <vector>
 
-int levenshteinDistance(std::string str1, std::string str2) {
-  std::vector<int> givenStringMap(26, 0);
-  std::vector<int> finalStringMap(26, 0);
-  int totalSubstitution{0};
-  totalSubstitution = std::abs(int(str2.size() - str1.size()));
-
-  for (const char &c : str1) {
-    givenStringMap.at(c - 97)++;
-  }
-  for (const char &c : str2) finalStringMap.at(c - 97)++;
-
-  for (int index = 0; index < 26; index++) {
-    if (finalStringMap.at(index) - givenStringMap.at(index) == 0) continue;
-    totalSubstitution++;
-  }
-  return totalSubstitution;
-}
+int levenshteinDistance(std::string str1, std::string str2) {}
 
 int main() {
   std::cout << levenshteinDistance("abc", "yabd");
