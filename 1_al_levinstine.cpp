fb912d6db0ca31a16e0e6c1c9af558192b556a5b
Aadil Srivastava <aadil.srivastava01@gmail.com>
2022-07-22 11:58:52 +0530

    added_space_optimsed_solution

--- 1_al_levinstine.cpp
+++ 1_al_levinstine.cpp
@@ -42,8 +42,45 @@ int levenshteinDistance(std::string str1, std::string str2) {
   return operationsTable.back().back();
 }
 
+int spaceOptlevenshteinDistance(std::string str1, std::string str2) {
+  auto small = str1.size() > str2.size() ? str2 : str1;
+  auto large = str1.size() > str2.size() ? str1 : str2;
+  auto small_size = small.size();
+  auto large_size = large.size();
+  int rowFlagindex = 1;
+
+  std::vector<std::vector<int>> operationsTable(
+      2, std::vector<int>(small_size + 1));
+
+  for (int colIndex = 0; colIndex < small_size + 1; colIndex++)
+    operationsTable[0][colIndex] = colIndex;
+
+  for (int rowIndex = 1; rowIndex < large_size + 1; rowIndex++) {
+    for (int colIndex = 0; colIndex < small_size + 1; colIndex++) {
+      if (colIndex == 0) {
+        operationsTable.at(rowFlagindex).at(colIndex) = rowIndex;
+        continue;
+      }
+      if (large.at(rowIndex - 1) == small.at(colIndex - 1))
+        operationsTable.at(rowFlagindex).at(colIndex) =
+            operationsTable.at(rowFlagindex - 1).at(colIndex - 1);
+      else {
+        operationsTable.at(rowFlagindex).at(colIndex) =
+            1 +
+            std::min(
+                std::min(operationsTable.at(rowFlagindex - 1).at(colIndex - 1),
+                         operationsTable.at(rowFlagindex).at(colIndex - 1)),
+                operationsTable.at(rowFlagindex - 1).at(colIndex));
+      }
+    }
+    operationsTable[0] = operationsTable[1];
+  }
+  return operationsTable.front().back();
+}
+
 int main() {
-  std::cout << levenshteinDistance("aabddy", "abc");
+  std::cout << levenshteinDistance("aabddy", "abc") << std::endl;
+  std::cout << spaceOptlevenshteinDistance("aabddy", "abc") << std::endl;
   std::cout << std::endl;
   return 0;
 }
