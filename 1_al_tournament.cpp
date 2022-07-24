8cdde7d2c3f5d2a34ee5189d02423b2ee5cfc96e
Aadil Srivastava <aadil.srivastava01@gmail.com>
2022-07-24 07:28:08 +0530

    added_new_solution

---
+++ 1_al_tournament.cpp
@@ -0,0 +1,22 @@
+/*
+
+Problem Link:
+
+
+*/
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::string tournamentWinner(std::vector<std::vector<std::string>> competitions,
+                             std::vector<int> results) {
+  // Write your code here.
+  return "";
+}
+
+int main() {
+  std::cout << std::endl;
+  return 0;
+}
