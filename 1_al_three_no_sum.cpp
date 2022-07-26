e04c4e2e57c36634fe6d1a5b086ee94e1decd8fd
Aadil Srivastava <aadil.srivastava01@gmail.com>
2022-07-26 23:57:05 +0530

    added_new_solution

--- 1_al_three_no_sum.cpp
+++ 1_al_three_no_sum.cpp
@@ -6,12 +6,47 @@ Problem Link:
 
 #include <algorithm>
 #include <iostream>
+#include <set>
 #include <string>
+#include <unordered_set>
 #include <vector>
 
 std::vector<std::vector<int>> threeNumberSum(std::vector<int> array,
                                              int targetSum) {
-  return {};
+  int size = array.size();
+  std::vector<int> triplet(3, 0);
+  std::set<std::vector<int>> triplets;
+
+  int remaningSum{0};
+  int indexBottom{0};
+  int min1{0}, min{0};
+  for (int indexTop = 0; indexTop < size; indexTop++) {
+    std::unordered_set<int> sumTracker;
+    remaningSum = targetSum - array.at(indexTop);
+    indexBottom = 0;
+    while (indexBottom < size) {
+      if (indexBottom == indexTop) {
+        indexBottom++;
+        continue;
+      }
+      if (sumTracker.find(remaningSum - array.at(indexBottom)) !=
+          sumTracker.end()) {
+        triplet.emplace_back(array.at(indexTop));
+        triplet.emplace_back(array.at(indexBottom));
+        triplet.emplace_back(remaningSum - array.at(indexBottom));
+        std::sort(triplet.begin(), triplet.end());
+        triplets.emplace(triplet);
+        triplet.clear();
+      }
+      sumTracker.insert(array.at(indexBottom));
+    }
+  }
+  std::vector<std::vector<int>> result(triplets.size());
+  for (auto triplet : triplets) {
+    result.emplace_back(triplet);
+  }
+  std::sort(result.begin(), result.end());
+  return result;
 }
 
 int main() {
