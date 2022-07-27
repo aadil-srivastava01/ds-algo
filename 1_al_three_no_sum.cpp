478a0523053d0c34a45f6554c86ef1c94aae509a
Aadil Srivastava <aadil.srivastava01@gmail.com>
2022-07-27 08:22:03 +0530

    added_problem_description

--- 1_al_three_no_sum.cpp
+++ 1_al_three_no_sum.cpp
@@ -1,7 +1,19 @@
 /*
 
 Problem Link:
+Write a function that takes in a non-empty array of distinct integers and an
+integer representing a target sum. The function should find all triplets in the
+array that sum up to the target sum and return a two-dimensional array of all
+these triplets. The numbers in each triplet should be ordered in ascending
+order, and the triplets themselves should be ordered in ascending order with
+respect to the numbers they hold. If no three numbers sum up to the target sum,
+the function should return an empty array.
 
+Sample input: Array: [12, 3, 1, 2,
+-6, 5, -8, 6]
+TargetSum: 0
+Sample Output: { -8, 2, 6 },{ -8, 3, 5  },{ -6, 1, 5
+}
 */
 
 #include <algorithm>
