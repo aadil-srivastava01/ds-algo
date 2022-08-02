8065910083f4d8a1e28cc7564ffffb76368ea4c1
Aadil Srivastava <aadil.srivastava01@gmail.com>
2022-08-02 22:55:43 +0530

    added_new_soln

---
+++ 1_al_moveToEnd.cpp
@@ -0,0 +1,17 @@
+/*
+
+Problem Link:
+
+*/
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::vector<int> moveElementToEnd(std::vector<int> array, int toMove) {}
+
+int main() {
+  std::cout << std::endl;
+  return 0;
+}
