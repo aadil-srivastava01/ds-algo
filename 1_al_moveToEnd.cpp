817c8c4e82dae1239ed04a0738d6e1a769ebc32f
Aadil Srivastava <aadil.srivastava01@gmail.com>
2022-08-03 18:02:16 +0530

    added_problem_desc

--- 1_al_moveToEnd.cpp
+++ 1_al_moveToEnd.cpp
@@ -1,7 +1,10 @@
 /*
 
 Problem Link:
-
+You're given an array of integers and an integer. Write a function that moves
+all instances of that integer in the array to the end of the array and returns
+the array. The function should perform this in place (i.e., it should mutate the
+input array) and doesn't need to maintain the order of the other integers.
 */
 
 #include <algorithm>
