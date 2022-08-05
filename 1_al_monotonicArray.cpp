501e0d2ffab3a0dc1ebeb6e93d01453d629bb247
Aadil Srivastava <aadil.srivastava01@gmail.com>
2022-08-05 23:46:14 +0530

    fixed_solution

--- 1_al_monotonicArray.cpp
+++ 1_al_monotonicArray.cpp
@@ -11,25 +11,37 @@ Problem Link:
 
 bool isMonotonic(std::vector<int> array) {
   if (array.empty()) return true;
-  auto size = array.size() - 1;
-  if (size < 1) return true;
+  auto size = array.size();
+  if (size < 2) return true;
+
   bool last, current;
-  if (array.at(1) == array.at(0))
-    last = (array.at(2) > array.at(1));
+  bool eqlFlag{false};
+  if (array.at(0) == array.at(1))
+    eqlFlag = true;
   else
     last = (array.at(1) > array.at(0));
-  for (int idx = 1; idx <= size - 1; idx++) {
-    current = (array.at(idx + 1) >= array.at(idx));
-    if (array.at(idx + 1) == array.at(idx)) current = last;
-    if (last != current) return false;
-    last = current;
+  size -= 1;
+  for (int idx = 1; idx < size; idx++) {
+    if (eqlFlag) {
+      if (array.at(idx + 1) != array.at(idx)) {
+        last = array.at(idx + 1) > array.at(idx);
+        eqlFlag = false;
+      }
+    } else {
+      current = (array.at(idx + 1) > array.at(idx));
+      if (array.at(idx + 1) == array.at(idx)) {
+        current = last;
+        continue;
+      }
+      if (last != current) return false;
+      last = current;
+    }
   }
   return true;
 }
 
 int main() {
-  std::vector<int> v1{-1, -1, -2, -3, -4, -5,  -5, -5,
-                      -6, -7, -8, -8, -9, -10, -11};
+  std::vector<int> v1{1, 1, 1, 2};
   std::cout << isMonotonic(v1);
   std::cout << std::endl;
   return 0;
