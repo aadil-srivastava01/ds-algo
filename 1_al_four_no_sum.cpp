b985ccd574a65df23d39e823833ae5a6f94ac389
Aadil Srivastava <aadil.srivastava01@gmail.com>
2022-08-14 16:30:40 +0530

    added_problem_statement

--- 1_al_four_no_sum.cpp
+++ 1_al_four_no_sum.cpp
@@ -3,6 +3,17 @@
 Problem Link:
 
 
+Write a function that takes in a non-empty array of distinct integers and an
+integer representing a target sum. The function should find all quadruplets in
+the array that sum up to the target sum and return a two-dimensional array of
+all these quadruplets in no particular order. If no four numbers sum up to the
+target sum, the function should return an empty array.
+Sample Input
+array = [7, 6, 4, −1, 1, 2]
+targetSum = 16
+Sample Output
+[[7,6,4,-1], [6,4,1,2]]
+
 */
 
 #include <algorithm>
