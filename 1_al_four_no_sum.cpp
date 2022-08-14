4f88b1c6a486e1f4ca22a2026a4390b162c8e857
Aadil Srivastava <aadil.srivastava01@gmail.com>
2022-08-14 16:30:26 +0530

    added_optimsed_solution

--- 1_al_four_no_sum.cpp
+++ 1_al_four_no_sum.cpp
@@ -2,6 +2,7 @@
 
 Problem Link:
 
+
 */
 
 #include <algorithm>
@@ -9,6 +10,7 @@ Problem Link:
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
+#include <utility>
 #include <vector>
 
 // O(n^3) Time, O(n) Space
@@ -43,9 +45,51 @@ std::vector<std::vector<int>> fourNumberSum(std::vector<int> array,
   return result;
 }
 
+// Optimised Solution
+// Space: O(n^2)
+// Time: Average: O(n^2), Worst: O(n^3)
+std::vector<std::vector<int>> fourNumberSumOpt(std::vector<int> array,
+                                               int targetSum) {
+  std::vector<std::vector<int>> result;
+  std::vector<int> temp;
+  auto size = array.size();
+  std::unordered_map<int, std::vector<std::vector<int>>> foundTable;
+  int sum{0};
+  if (array.empty()) return result;
+  for (int outIdx = 0; outIdx < size; outIdx++) {
+    for (int inIdx1 = outIdx + 1; inIdx1 < size; inIdx1++) {
+      if (foundTable.find(targetSum - (array.at(outIdx) + array.at(inIdx1))) !=
+          foundTable.end()) {
+        for (auto &item :
+             foundTable.at(targetSum - (array.at(outIdx) + array.at(inIdx1)))) {
+          temp.emplace_back(item.at(0));
+          temp.emplace_back(item.at(1));
+          temp.emplace_back(array.at(outIdx));
+          temp.emplace_back(array.at(inIdx1));
+          result.emplace_back(temp);
+          temp.clear();
+        }
+      }
+    }
+    for (int inIdx2 = 0; inIdx2 < outIdx; inIdx2++) {
+      sum = array.at(outIdx) + array.at(inIdx2);
+      if (foundTable.find(sum) != foundTable.end())
+        foundTable.at(sum).emplace_back(
+            std::vector<int>{array.at(outIdx), array.at(inIdx2)});
+      else {
+        foundTable.emplace(
+            std::make_pair(sum, std::vector<std::vector<int>>{
+                                    {array.at(outIdx), array.at(inIdx2)}}));
+      }
+    }
+  }
+  return result;
+}
+
 int main() {
   std::vector<int> v1{7, 6, 4, -1, 1, 2};
-  auto res = fourNumberSum(v1, 16);
+  //   auto res = fourNumberSum(v1, 16);
+  auto res = fourNumberSumOpt(v1, 16);
   std::cout << std::endl;
   return 0;
 }
