45a331ac0a01589e19f1da5d986627a9a1a4b2a7
Aadil Srivastava <aadil.srivastava01@gmail.com>
2022-08-25 09:48:50 +0530

    added_problem_statement

---
+++ 1_al_merge_interval.cpp
@@ -0,0 +1,22 @@
+/*
+
+Problem Link:
+Write a function that takes in a non-empty array of arbitrary intervals, merges
+any overlapping intervals, and returns the new intervals in no particular order.
+Each interval interval is an array of two integers, with interval[0] as the
+start of the interval and interval [1] as the end of the interval. Note that
+back-to-back intervals aren't considered to be overlapping. For example, [1, 5]
+and [6, 7] aren't overlapping; however, [1, 6] and [6, 7] are indeed
+overlapping. Also note that the start of any particular interval will always be
+less than or equal to the end of that interval.
+*/
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+  std::cout << std::endl;
+  return 0;
+}
